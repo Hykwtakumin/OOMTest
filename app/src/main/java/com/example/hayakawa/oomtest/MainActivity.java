9382f5d1c5ecfd6ecd95ed0b21d0c5eb404c81be
hykwtakumin <hykwtakumin@gmail.com>
2016-04-05 08:56:47 +0900

    Javaヒープを圧迫する処理をFloatingButtonに記述
    参考：http://d.hatena.ne.jp/terazzo/20130320/1363731368

--- app/src/main/java/com/example/hayakawa/oomtest/MainActivity.java
+++ app/src/main/java/com/example/hayakawa/oomtest/MainActivity.java
@@ -22,8 +22,10 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                StringBuilder sb = new StringBuilder();
+                while (true) {
+                    sb.append("1234567890");
+                }
             }
         });
     }
