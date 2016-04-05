1ba53c8d0c3a17f3d89d1e717b8e148286bd0131
hayakawa <hykwtakumin@gmail.com>
2016-04-05 18:07:20 +0900

    不要なsnackbarを削除。

--- app/src/main/java/com/example/hayakawa/oomtest/MainActivity.java
+++ app/src/main/java/com/example/hayakawa/oomtest/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.hayakawa.oomtest;
 import android.os.Bundle;
 import android.os.Debug;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
